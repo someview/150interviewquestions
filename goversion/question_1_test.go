c4c007d504ababfe8b1857dd9c8f51c4dda37519
unknown <1184618238@qq.com>
2024-01-14 04:01:35 +0800

    fix:完成题目1

---
+++ goversion/question_1_test.go
@@ -0,0 +1,11 @@
+package interview
+
+import "testing"
+
+func TestMergemergeSortedArray(t *testing.T) {
+	nums1 := []int{2, 0}
+	m := 1
+	nums2 := []int{1}
+	n := 1
+	mergeSortedArray(nums1, m, nums2, n)
+}
