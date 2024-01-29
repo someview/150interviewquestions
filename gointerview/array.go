334b138d122057cb94cb93e0428441d45421cf99
unknown <1184618238@qq.com>
2024-01-29 20:29:08 +0800

    fix:添加测试

--- gointerview/array.go
+++ gointerview/array.go
@@ -289,26 +289,3 @@ func maxArea(height []int) int {
 	}
 	return res
 }
-
-func minSubArrayLen(target int, nums []int) int {
-	n := len(nums)
-	if n == 0 {
-		return 0
-	}
-	start, end := 0, 0
-	sum := 0
-	ans := n + 1
-	for end < target {
-		sum += nums[end]
-		for sum >= target {
-			ans = min(ans, end-start+1)
-			sum -= nums[start]
-			start++
-		}
-		end++
-	}
-	if ans == n+1 {
-		ans = 0
-	}
-	return ans
-}
